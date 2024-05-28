c8dd889690bea941b529cc6c7704e35f6de0bd75
cama0000 <acama0000@gmail.com>
2024-05-27 19:14:44 -0500

    initial commit

---
+++ schoolapp-api/src/main/java/com/c5r/schoolapp_api/SchoolappApiApplication.java
@@ -0,0 +1,13 @@
+package com.c5r.schoolapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolappApiApplication.class, args);
+	}
+
+}
---
+++ schoolapp-api/src/test/java/com/c5r/schoolapp_api/SchoolappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c5r.schoolapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
