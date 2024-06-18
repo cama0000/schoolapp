cd77638b7cf7813f1aa6185a62007131d3153a10
cama0000 <acama0000@gmail.com>
2024-06-18 18:15:10 -0500

    make sidebar look nicer

--- schoolapp-frontend/src/components/SideBar.jsx
+++ schoolapp-frontend/src/components/SideBar.jsx
@@ -64,70 +64,6 @@
 // export default SideBar;
 
 
-// import React from 'react';
-// import Link from 'next/link';
-// import { useRouter } from 'next/router';
-// import { sideBarLinks } from '@/constants';
-// import { useAuth } from '@/context/AuthContext';
-// import { Button } from '@mui/material';
-// import { toast } from 'react-toastify';
-
-// const SideBar = () => {
-//   const router = useRouter();
-//   const { student, logout } = useAuth();
-
-//   const handleSettingsClick = () => {
-//     router.push('/settings');
-//   };
-
-//   const handleLogoutClick = () => {
-//     toast.success("Logout Successful!");
-//     logout();
-//   };
-
-//   return (
-//     <div className='fixed top-0 left-0 h-full border-r border-blue-300'>
-//       <div className='flex flex-col gap-11 bg-purple-500 pr-11 pl-5 py-5 h-full'>
-//         <Link href="/" className="flex gap-3 items-center">
-//           Wave
-//         </Link>
-
-//         <div>
-//           Profile Pic
-//         </div>
-
-//         <div>
-//           {student?.username}
-//         </div>
-
-//         <ul className="flex flex-col gap-6">
-//           {sideBarLinks.map((link, index) => (
-//             <li key={index}>
-//               <Link href={link.href} className="flex items-center gap-3">
-//                 <img src={link.imgURL} alt={link.label} className='w-6 h-6' />
-//                 <span className="text-white hover:text-black cursor-pointer">{link.label}</span>
-//               </Link>
-//             </li>
-//           ))}
-//           <li className="px-4 py-2 flex items-center gap-2" onClick={handleSettingsClick}>
-//             <img src="/assets/images/settings.png" alt="settings" className='w-4 h-4' />
-//             <span className="text-white hover:text-black cursor-pointer">Settings</span>
-//           </li>
-//           <li className="px-4 py-2 flex items-center gap-2" onClick={handleLogoutClick}>
-//             <Button className='text-white hover:text-black'>
-//               Logout
-//             </Button>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SideBar;
-
-
-
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -173,14 +109,16 @@ const SideBar = () => {
               </Link>
             </li>
           ))}
-          <li className="px-4 py-2 flex items-center gap-2" onClick={handleSettingsClick}>
-            <img src="/assets/images/settings.png" alt="settings" className='w-4 h-4' />
+          
+          <li className="flex items-center gap-3 mt-48" onClick={handleSettingsClick}>
+            <img src="/assets/images/settings.png" alt="settings" className='w-6 h-6' />
             <span className="text-white hover:text-black cursor-pointer">Settings</span>
           </li>
-          <li className="px-4 py-2 flex items-center gap-2" onClick={handleLogoutClick}>
-            <Button className='text-white hover:text-black'>
+
+          <li className="flex items-center gap-9" onClick={handleLogoutClick}>
+            <span className='ml-5 text-white hover:text-black cursor-pointer'>
               Logout
-            </Button>
+            </span>
           </li>
         </ul>
       </div>
