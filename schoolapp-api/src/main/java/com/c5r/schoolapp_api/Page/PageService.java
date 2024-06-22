038f2c07ec1376dfbc3f5a42aed386d06e220651
cama0000 <acama0000@gmail.com>
2024-06-22 16:26:37 -0500

    popup to create page

--- schoolapp-api/src/main/java/com/c5r/schoolapp_api/Page/PageService.java
+++ schoolapp-api/src/main/java/com/c5r/schoolapp_api/Page/PageService.java
@@ -8,5 +8,5 @@ public class PageService {
     @Autowired
     private PageRepository pageRepository;
 
-    
+
 }
--- schoolapp-frontend/src/components/CourseSideBar.jsx
+++ schoolapp-frontend/src/components/CourseSideBar.jsx
@@ -1,11 +1,35 @@
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
+import { toast } from 'react-toastify';
 
 const CourseSideBar = () => {
   const router = useRouter();
   const { course } = useAuth();
+  const [isOpen, setOpen] = useState(false);
+  const [title, setTitle] = useState(false);
+
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+
+    // const task = { taskName, description, deadline: deadline, courseId: selectedCourse?.id, studentId: student?.id}
+    // await addTask(task);
+    // fetchTasks();
+    handleClose();
+
+    toast.success("Page created successfully!");
+  }
 
   return (
     <div className='hidden md:flex flex-col ml-7 h-full w-48 bg-purple-300 text-white'>
@@ -27,8 +51,54 @@ const CourseSideBar = () => {
         <span>
           Notebook
         </span>
-        <AddIcon style={{color: 'gray', width: '15px', cursor: 'pointer'}}/>
+        <AddIcon style={{color: 'gray', width: '15px', cursor: 'pointer'}} onClick={handleOpen}/>
       </div>
+
+
+
+
+      {/* CREATE PAGE */}
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        component="form" validate="true" onSubmit={handleSubmit}
+        disableScrollLock={true}
+      >
+        <DialogTitle>Create Page</DialogTitle>
+        <DialogContent>
+
+            <DialogContentText>
+                Create a page.
+            </DialogContentText>
+
+            <TextField
+                autoFocus
+                required
+                margin="dense"
+                id="title"
+                name="title"
+                label="Title"
+                type="string"
+                fullWidth
+                variant="standard"
+                onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <br/>
+            <br/>
+
+        </DialogContent>
+
+        <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button variant="contained" type="submit" onSubmit={handleSubmit} color="primary">Create</Button>
+        </DialogActions>
+
+    </Dialog>
+
+
+
+
     </div>
   );
 }
